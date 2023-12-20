6dd3183f9b3ac744626b44743851ff77ea72e376
andreym <andreymichaely@gmail.com>
2023-12-20 12:38:33 +0200

    https://github.com/d-stuff/netcraft-08-2023/issues/6 fixed after David guideline

--- index.js
+++
@@ -1,7 +0,0 @@
-import {renderStudentsList} from "./ui/student-list/students-list.js";
-import {initStudents} from "./app/students-list/students-init.js";
-
-document.addEventListener("DOMContentLoaded", function(){
-    initStudents();
-    renderStudentsList();
-});
