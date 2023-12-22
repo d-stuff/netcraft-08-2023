0947c229d6d4be85c99136fa9f19cfa53e2e77db
TheLastVigil <idanflishman@gmail.com>
2023-12-22 16:48:12 +0200

    added search input filter

---
+++ inputField.js
@@ -0,0 +1,12 @@
+const studentItems = document.querySelectorAll('.student-item');
+const searchInput = document.querySelector('input[type="search"]');
+
+searchInput.addEventListener('input', () => {
+    studentItems.forEach((studentItem) => {
+        if (!studentItem.innerHTML.toLowerCase().includes(searchInput.value.toLowerCase())) {
+            studentItem.style.display = 'none';
+        } else {
+            studentItem.style.display = 'block';
+        }
+    });
+});
