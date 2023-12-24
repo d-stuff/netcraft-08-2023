d0db14c23ad662a5084e44566cdc052af68e20ed
eyal <eyalkeila@gmail.com>
2023-12-24 23:56:51 +0200

    eyal CV

--- inputField.js
+++ inputField.js
@@ -3,7 +3,8 @@ const searchInput = document.querySelector('input[type="search"]');
 
 searchInput.addEventListener('input', () => {
     studentItems.forEach((studentItem) => {
-        if (!studentItem.innerHTML.toLowerCase().includes(searchInput.value.toLowerCase())) {
+        const name = studentItem.querySelector('h4').innerHTML;
+        if (!name.toLowerCase().includes(searchInput.value.toLowerCase())) {
             studentItem.style.display = 'none';
         } else {
             studentItem.style.display = 'block';
